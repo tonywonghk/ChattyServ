c6f6445a66f7f2dc55cf65966e24c6594f4f2da8
tony_inl <tony@inl.me>
2015-01-31 21:06:59 +0800

    Basic WebSocket Handling is completed

--- src/main/java/me/inl/chatty/serv/TextWebSocketFrameHandler.java
+++ src/main/java/me/inl/chatty/serv/TextWebSocketFrameHandler.java
@@ -1,16 +1,20 @@
 package me.inl.chatty.serv;
 
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import java.util.logging.Logger;
 
 /**
  * Created by Tony Wong on 31/1/15.
  */
 public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>{
     private final ChannelGroup group;
+    private static final Logger log = Logger.getLogger(TextWebSocketFrameHandler.class.getName());
+
 
     public TextWebSocketFrameHandler(ChannelGroup group) {
         this.group = group;
@@ -20,16 +24,20 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if (evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
             ctx.pipeline().remove(HttpRequestHandler.class);
+
             group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
             group.add(ctx.channel());
-        } else {
+
+        }else {
             super.userEventTriggered(ctx, evt);
         }
     }
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
-        group.writeAndFlush(msg.retain());
+        String resp = "{'text':"+ msg.text() +"}";
+        log.info("Server Response["+ group.size() +"]:" + resp);
+        group.writeAndFlush(new TextWebSocketFrame(resp));
     }
 
 }
